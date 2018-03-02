fdca7ad80b892be895e49bdd8da0ab995ad31dec
sanjsanj <sanjsanj@hotmail.com>
2018-03-02 14:32:49 +0000

    Begin wiring in redux

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './styles/index.css';
-import App from './App';
+import combinedReducers from './reducers';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(combinedReducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
+
 registerServiceWorker();
