f818699ff2f66bb17327a4d0407bd2c994ed90e8
sanjsanj <sanjsanj@hotmail.com>
2018-03-02 15:03:51 +0000

    Dummy Teams markup

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import Header from './header';
+import Header from './Header';
+import Teams from './Teams';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Header />
+        <Teams />
         <p className="app-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/Teams.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Teams = () => (
+  <ul className="container nav nav-tabs">
+    <li className="nav-item">
+      <a className="nav-link active" href="#">Active</a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link" href="#">Link</a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link" href="#">Link</a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link disabled" href="#">Disabled</a>
+    </li>
+  </ul>
+);
+
+export default Teams;
