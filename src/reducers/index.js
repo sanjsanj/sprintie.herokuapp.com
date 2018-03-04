c7395dc5d9fee1d32676607bc878c11009c3428f
sanjsanj <sanjsanj@hotmail.com>
2018-03-04 18:34:09 +0000

    Remove unneeded reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-// import { teamReducer } from './teamReducer';
 import { appReducer } from './appReducer';
 
 const reducers = combineReducers({
   appReducer,
-  // teamReducer,
 });
 
 export default reducers;
