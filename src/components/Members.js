6e6e68c6b64496679ab2323b5f12c25f6afcb7a4
sanjsanj <sanjsanj@hotmail.com>
2018-03-04 14:07:24 +0000

    Table container layout

--- src/components/Members.js
+++ src/components/Members.js
@@ -4,28 +4,30 @@ import actions from '../actions';
 import { getSelectedTeam } from '../helpers';
 
 const Members = ({ selectedTeam }) => (
-  <table className="container table table-striped">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Days off this sprint</th>
-      </tr>
-    </thead>
-    <tbody className="">
-      {selectedTeam.members.map(member => (
-        <tr key={member.name}>
-          <td>
-            <button className="btn btn-light">{member.name}</button>
-          </td>
-          <td>
-            <div className="">
-              <input className="" type="number" placeholder="0" step=".5" min="0" max="10" />
-            </div>
-          </td>
+  <div className="container">
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Days off this sprint</th>
         </tr>
+      </thead>
+      <tbody className="">
+        {selectedTeam.members.map(member => (
+          <tr key={member.name}>
+            <td>
+              <button className="btn btn-light">{member.name}</button>
+            </td>
+            <td>
+              <div className="">
+                <input className="" type="number" placeholder="0" step=".5" min="0" max="10" />
+              </div>
+            </td>
+          </tr>
       ))}
-    </tbody>
-  </table>
+      </tbody>
+    </table>
+  </div>
 );
 
 function mapStateToProps(state) {
