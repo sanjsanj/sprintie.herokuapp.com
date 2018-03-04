0ae90c07a8807284f80c0e9d6b712698a9a67385
sanjsanj <sanjsanj@hotmail.com>
2018-03-04 17:45:29 +0000

    file name swap

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './Header.js';
+import Header from './Header';
 import Teams from './Teams';
 import TeamSettingsToggle from './TeamSettingsToggle';
 import TeamSettings from './TeamSettings';
