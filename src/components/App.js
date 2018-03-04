00f8f303491ce41d5f195aa4d9179b9d23aa4bc8
sanjsanj <sanjsanj@hotmail.com>
2018-03-04 17:42:11 +0000

    Remove css, change build task

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './Header';
+import Header from './Header.js';
 import Teams from './Teams';
 import TeamSettingsToggle from './TeamSettingsToggle';
 import TeamSettings from './TeamSettings';
