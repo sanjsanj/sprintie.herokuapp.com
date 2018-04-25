6560bcd86556808d04bcc17fa9eb67ae1c13adb4
Sanjay Purswani <sanjsanj@hotmail.com>
2018-04-25 10:47:13 +0100

    Add Cypress for E2E tests

---
+++ cypress/integration/website_spec.js
@@ -0,0 +1,17 @@
+describe('Kitchen Sink', () => {
+  it('.should() - assert that <title> is correct', () => {
+    // https://on.cypress.io/visit
+    cy.visit('http://localhost:3001/');
+
+    // Here we've made our first assertion using a '.should()' command.
+    // An assertion is comprised of a chainer, subject, and optional value.
+
+    // https://on.cypress.io/should
+    // https://on.cypress.io/and
+
+    // https://on.cypress.io/title
+    cy.title().should('include', 'Sprintie!');
+    //   ↲               ↲            ↲
+    // subject        chainer      value
+  });
+});
