cc30b66191091d796b02e914201af62b4385ffe7
Stacey Fenton <staceyfenton@gmail.com>
2018-04-25 15:40:34 +0100

    Fix typos in member test

--- cypress/integration/members_spec.js
+++ cypress/integration/members_spec.js
@@ -7,34 +7,33 @@ describe('Sprintie', () => {
         cy.get('.nav-link').click();
         });
 
-        it('should add a new row in the memeber table when Add Memeber button is clicked', () => {            
+        it('should add a new row in the member table when Add Member button is clicked', () => {            
             cy.get('.table-striped tbody').children().should('have.length', 0);
             cy.get('#btn-add-member').click();
             cy.get('.table-striped tbody').children().should('have.length', 1);
             cy.get('#btn-add-member').click();
             cy.get('.table-striped tbody').children().should('have.length', 2);
         });
-        context('When Add Memeber button is clicked', () => {
+        context('When Add Member button is clicked', () => {
             beforeEach(() => {
                 cy.get('#btn-add-member').click();
             });
             it('should update the Name when type while the input is focus', () => {
                 cy.get('.memberName').click().type('test')
-                .should('have.attr', 'placeholder', 'test')
+                .should('have.attr', 'placeholder', 'test');
             });
-            it('should Delete a row in the memeber table when Delete button is clicked', () => {
+            it('should Delete a row in the member table when Delete button is clicked', () => {
                 cy.get('.table-striped tbody').children().should('have.length', 1);
                 cy.contains('Delete').click();
-                cy.get('.table-striped tbody').children().should('have.length', 0);                
-
+                cy.get('.table-striped tbody').children().should('have.length', 0);
             });
-            it('should let us set evevery sprint days off for each memeber', () => {
+            it('should let us set evevery sprint days off for each member', () => {
                 cy.get('.day-off-every-sprint').click()
                 .type('1')
-                .should('have.attr', 'placeholder', '1')
+                .should('have.attr', 'placeholder', '1');
     
             });
-            it('should let us set this sprint days off for each memeber', () => {
+            it('should let us set this sprint days off for each member', () => {
                 cy.get('.day-off-this-sprint').click()
                 .type('1')
                 .should('have.attr', 'placeholder', '1');
